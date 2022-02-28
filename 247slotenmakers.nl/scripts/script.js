4f3dc1bb52e118b58c29fc67447cb27d70e8abe1
Sergey-develop <84603174+Sergey-develop@users.noreply.github.com>
2022-02-28 09:44:17 +0300

    add landing

---
+++ 247slotenmakers.nl/scripts/script.js
@@ -0,0 +1,49 @@
+$(document).on('ready', function () {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        infinite: true,
+        arrows: false,
+        dotsClass: "my-dots",
+        dots: true,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
